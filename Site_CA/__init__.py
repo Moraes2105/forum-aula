ab5baf27f7be6194baa97ba41486887e8f34e25e
Moraes2105 <66483782+Moraes2105@users.noreply.github.com>
2025-01-28 11:25:55 -0300

    Update __init__.py

--- Site_CA/__init__.py
+++ Site_CA/__init__.py
@@ -2,11 +2,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'e65a35b63e25833f2cf2c880306481ff'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site_CA.db'
+if os.getenv('DATABASE_URL'):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site_CA.db'
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
@@ -15,11 +19,3 @@ login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-info'
 
 from Site_CA import routes
-
-
-
-# GERADOR DE SECRET_KEY:
-# No TERMINAL:
-# Python (enter)
-# Import secrets (enter)
-# Secrets.token_hex(16) (enter)		-	no (16) insira a quantidade desejada de caracteres.
